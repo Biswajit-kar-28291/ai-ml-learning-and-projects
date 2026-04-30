8b0c3a9857e15af1574c743ba746ba10b7e4de25
Biswajit Kar <bkar28291@gmail.com>
2026-04-30 12:10:59 +0530

    Add complete linear regression module with math, gradient descent, scratch implementation, and sklearn version

--- 06-Linear-Regression/src/04_sklearn_linear_regression.py
+++ 06-Linear-Regression/src/04_sklearn_linear_regression.py
@@ -1,1 +1,17 @@
-# 
+import pandas as pd 
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
+data=pd.read_csv("06-Linear-Regression\data\salary_data.csv")
+
+x=data[['experience']]
+y=data['salary']
+
+# X_train,,y_train
+
+model=LinearRegression()
+model.fit(x,y)
+
+a=model.predict([[12]])
+print(a)
+
